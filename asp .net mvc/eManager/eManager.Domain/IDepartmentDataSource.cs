4a7c4ed48b9b453dd57bb954ad5cdb72d36a9496
p.torchilov <p.torchilov@gmail.com>
2015-03-09 23:05:19 +0300

    Add domain solution.

---
+++ asp .net mvc/eManager/eManager.Domain/IDepartmentDataSource.cs
@@ -0,0 +1,11 @@
+﻿namespace eManager.Domain
+{
+    using System.Linq;
+
+    public interface IDepartmentDataSource
+    {
+        IQueryable<Employee> Employees { get; set; }
+
+        IQueryable<Department> Departments { get; set; }
+    }
+}
