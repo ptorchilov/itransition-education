322f8e6421b4931a0b14d06ae436ccd55acb84f0
p.torchilov <p.torchilov@gmail.com>
2015-04-29 01:58:42 +0300

    Add XML console client.

---
+++ asp .net mvc/WebApiSolution/ConsoleClient/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ConsoleClient
+{
+    using System;
+    using System.Net;
+    using System.Net.Http;
+    using System.Net.Http.Headers;
+    using System.Xml.Linq;
+
+    public class Program
+    {
+        private const String CurrentApiUrl = "http://localhost:50013/";
+
+        public static void Main()
+        {
+            var client = new HttpClient();
+
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/xml"));
+
+            var result = client.GetAsync(new Uri(CurrentApiUrl.TrimEnd('/') + "/api/videos")).Result;
+
+            if (result.StatusCode == HttpStatusCode.OK)
+            {
+                var doc = XDocument.Load(result.Content.ReadAsStreamAsync().Result);
+                var ns = (XNamespace)"http://schemas.datacontract.org/2004/07/WebApiProject.Models";
+
+                foreach (var title in doc.Descendants(ns + "Title"))
+                {
+                    Console.WriteLine(title.Value);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
