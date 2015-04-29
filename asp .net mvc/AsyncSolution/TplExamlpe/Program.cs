1fe2c40a48201d99ca163cc26af9cdf922f0495e
p.torchilov <p.torchilov@gmail.com>
2015-04-29 13:55:36 +0300

    Add example for threads.

---
+++ asp .net mvc/AsyncSolution/TplExamlpe/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace TplExamlpe
+{
+    using System;
+    using System.Threading;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var result = SlowOpernation();
+
+            for (var i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("Slow operation result: {0}", result);
+            Console.WriteLine("Main complete on {0}", 
+                Thread.CurrentThread.ManagedThreadId);
+        }
+
+        private static int SlowOpernation()
+        {
+            Console.WriteLine("Slow operation started on {0}",
+                Thread.CurrentThread.ManagedThreadId);
+            Thread.Sleep(2000);
+            Console.WriteLine("Slow operation complete on {0}",
+                Thread.CurrentThread.ManagedThreadId);
+
+            return 42;
+        }
+    }
+}
